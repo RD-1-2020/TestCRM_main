a829a8cf4a9377bbf1ea232dfbe4fe908d549a2b
RD-1-2020 <mestrx@mail.ru>
2021-01-25 11:29:22 +0700

    All is in one a namespace and all is work

--- TestCRM/SupplyTest.cs
+++ TestCRM/SupplyTest.cs
@@ -3,52 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using TestCRM;
 using OpenQA.Selenium;
-using Prj_test.PageObjects;
+using PageObjSpace;
 using System.Threading;
 using NUnit.Framework;
 
 namespace Prj_test
 {
-    class SupplyTest
-    {
-        private IWebDriver driver;
-        [SetUp]
-        public void Setup()
-        {
-            driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
-            driver.Navigate().GoToUrl("http://crm.sibelcom.tech/");
-            driver.Manage().Window.Maximize();
-        }
-        [Test]
-        public void Test1()
-        {
-            LoginPage_PageObj login_Form = new LoginPage_PageObj(driver);
-            //Create deal 
-            login_Form.Sign_in(0)
-                .go_to_deal()
-                .New_Deal()
-                .fill_newdeal_form()
-                .add_deal()
-                .add_product()//Реализовать превязку каждого цикла добавления товаров к длинне массива с товарами
-                .add_product_list()
-                .fill_list()
-                .request_supply()
-                .send_request();
-            //Execute id deals
-            string deal_id = driver.FindElement(By.XPath("//input[@data-id='" + driver.Url.Substring(driver.Url.LastIndexOf("=") + 1) + "']")).GetAttribute("value");
-            //Work with deal from it id on a deal page
-            (new NewDealPage_PageObj(driver))
-                .exit()
-                .Sign_in(1)
-                .go_to_supply(deal_id)
-                .open_supply_deal()
-                .take_in_work();
-        }
-        [TearDown]
-        public void TearDown()
-        {
-            driver.Quit();
-            //закрытие
-        }
-    }
+    
 }
---
+++ TestCRM/Tests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using PageObjSpace;
+using System.Threading;
+namespace TestCRM
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+            driver.Navigate().GoToUrl("http://crm.sibelcom.tech/");
+            driver.Manage().Window.Maximize();
+        }
+        [Test]
+        public void Login_Test()
+        {
+            LoginPage_PageObj login_Form = new LoginPage_PageObj(driver);
+            for (int i = 0; i < login_Form.Length; i++)
+            {
+                try
+                {
+                    login_Form
+                    .Sign_in(i)
+                    .exit()
+                    ;
+                }
+                catch (OpenQA.Selenium.NoSuchElementException)
+                {
+                    Assert.IsTrue(false, LoginPage_PageObj.Logs.arr_data[i].email + " " + LoginPage_PageObj.Logs.arr_data[i].email + " - is not valid data!");
+                }
+            }
+            Assert.Pass();
+        }
+        [Test]
+        public void NewDealTest()
+        {
+            new LoginPage_PageObj(driver).Sign_in(0)
+                .go_to_deal()
+                .New_Deal()
+                .fill_newdeal_form()
+                .add_deal()
+                .add_product()
+                .add_product_list()
+                .fill_list();
+            Assert.Pass();
+        }
+        [Test]
+        public void SupplyTest()
+        {
+            LoginPage_PageObj login_Form = new LoginPage_PageObj(driver);
+            //Create deal 
+            login_Form.Sign_in(0)
+                .go_to_deal()
+                .New_Deal()
+                .fill_newdeal_form()
+                .add_deal()
+                .add_product()//Реализовать превязку каждого цикла добавления товаров к длинне массива с товарами
+                .add_product_list()
+                .fill_list()
+                .request_supply()
+                .send_request();
+            //Execute id deals
+            string deal_id = driver.FindElement(By.XPath("//input[@data-id='" + driver.Url.Substring(driver.Url.LastIndexOf("=") + 1) + "']")).GetAttribute("value");
+            //Work with deal from it id on a deal page
+            (new NewDealPage_PageObj(driver))
+                .exit()
+                .Sign_in(1)
+                .go_to_supply(deal_id)
+                .open_supply_deal()
+                .take_in_work();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            //закрытие
+        }
+    }
+}
